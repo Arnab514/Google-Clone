1bd70986735bfdd10806e8da667c2b78dedf871b
Arnab Majumder <arnabmajumdar399@gmail.com>
2024-12-23 15:30:52 +0530

    eslint disabled

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ["images.unsplash.com", "www.google.com"]
     },
+    eslint: {
+        ignoreDuringBuilds: true, // Skips ESLint checks during builds
+      },
     reactStrictMode: true,
 }
 
